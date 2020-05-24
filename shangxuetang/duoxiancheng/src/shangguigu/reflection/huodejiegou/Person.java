ca6625f05f2a845d8ed0839a5c021a9e5336b70d
lizhenhong <18732068538@163.com>
2020-05-24 15:34:42 +0800

    增加person类的getter/setter方法

--- shangxuetang/duoxiancheng/src/shangguigu/reflection/huodejiegou/Person.java
+++ shangxuetang/duoxiancheng/src/shangguigu/reflection/huodejiegou/Person.java
@@ -50,6 +50,30 @@ public class Person extends Creature<String> implements Comparable<String>, MyIn
         System.out.println("调用静态方法的时候后加的");
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
