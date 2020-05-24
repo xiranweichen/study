9b969865597ff9b4c387b3786feeb48c68b721f0
lizhenhong <18732068538@163.com>
2020-05-24 15:00:19 +0800

    修改路径，将尚硅谷的和腾讯课堂的分开

---
+++ shangxuetang/duoxiancheng/src/shangguigu/duotai/Person.java
@@ -0,0 +1,21 @@
+package shangguigu.duotai;
+
+/**
+ * @author: lizhenhong
+ * @description: 父类
+ * @date: 2020/1/21 15:20
+ */
+public class Person {
+
+    private String name;
+    private int age;
+
+    public void eat() {
+        System.out.println("=======>eat 饭");
+    }
+
+    public void walk() {
+        System.out.println("=======> walk 路");
+    }
+
+}
